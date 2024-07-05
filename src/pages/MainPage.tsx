053d955ca40e73dc0e0483d0166113ef6bb7b4ff
Márcio Mingante <marciomvp17@gmail.com>
2024-07-04 21:52:12 -0300

    adiciona logica de paginação

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,11 +3,28 @@ import { useLocation } from "react-router-dom";
 import { getNewsInfo } from "../services/api";
 import { ItemType } from "../types/types";
 import NewsCard from "../components/NewsCard";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 function MainPage() {
   const [page, setPage] = useState(1);
   const [news, setNews] = useState<ItemType[]>([]);
   const { pathname } = useLocation();
+
+  const handlePrevPage = async () => {
+    const currentPage = page === 1 ? 1 : page - 1;
+
+    const data = await getNewsInfo(pathname, currentPage);
+    setPage(currentPage);
+    setNews(data.items);
+  }
+  
+  const handleNextPage = async () => {
+    const currentPage = page + 1;
+
+    const data = await getNewsInfo(pathname, currentPage);
+    setPage(currentPage);
+    setNews(data.items);
+  }
 
   const getFirstList = async () => {
     const data = await getNewsInfo(pathname, 1);
@@ -32,6 +49,21 @@ function MainPage() {
           {news.map((item, index) => (
             <NewsCard key={ index } item={ item } />
           ))}
+        </div>
+
+          {/* div para paginação */}
+        <div className="flex justify-around my-6">
+          <button
+            onClick={ handlePrevPage }
+          >
+            <ChevronLeft />
+          </button>
+          
+          <button
+            onClick={ handleNextPage }
+          >
+            <ChevronRight />
+          </button>
         </div>
       </section>
     </main>
